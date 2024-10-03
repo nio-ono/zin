dfb7ea82c8a55b5b1e8ed581011120022e596488
Nio Ono <me@nioono.com>
2024-10-03 16:09:45 -0700

    Fix dynamic import of globals.js and prevent EEXIST errors with build locking and overwrite options.

--- source/globals.js
+++ source/globals.js
@@ -1,6 +1,6 @@
 module.exports = {
     site: {
-        name: "Untitled Zin Project",
+        name: "Untitled asdf",
         description: "In the quiet of dawn, the world holds its breath, awaiting a new beginning.",
     }
 };
