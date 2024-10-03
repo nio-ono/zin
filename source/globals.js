0bd8de7187db71d23d81267589a86b49cbdd7e23
Nio Ono <me@nioono.com>
2024-10-03 16:10:38 -0700

    Fixed test content

--- source/globals.js
+++ source/globals.js
@@ -1,6 +1,6 @@
 module.exports = {
     site: {
-        name: "Untitled asdf",
+        name: "Untitled Zin Project",
         description: "In the quiet of dawn, the world holds its breath, awaiting a new beginning.",
     }
-};
+};``
